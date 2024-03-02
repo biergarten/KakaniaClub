a4bde4693ccebf6572cbe526e268c9f6e2768f46
Francisco de Santiago <jofrasa@hotmail.com>
2024-03-02 22:20:42 +0000

    added ui with msal

--- members-management-ui-2/src/app/app.module.ts
+++ members-management-ui-2/src/app/app.module.ts
@@ -1,22 +1,77 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http"; // Import
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { ProfileComponent } from "./profile/profile.component";
+
+import {
+  MsalModule,
+  MsalRedirectComponent,
+  MsalGuard,
+  MsalInterceptor,
+} from "@azure/msal-angular"; // Import MsalInterceptor
+import {
+  InteractionType,
+  PublicClientApplication,
+} from "@azure/msal-browser";
+
+const isIE =
+  window.navigator.userAgent.indexOf("MSIE ") > -1 ||
+  window.navigator.userAgent.indexOf("Trident/") > -1;
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ProfileComponent
-  ],
+  declarations: [AppComponent, HomeComponent, ProfileComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    HttpClientModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        auth: {
+          clientId: "27aa542e-e874-45b8-8091-128794c543f4",
+          authority:
+            "https://login.microsoftonline.com/0f027ae1-08fc-4613-ab83-6848eb9f69db",
+          redirectUri: "http://localhost:4200",
+        },
+        cache: {
+          cacheLocation: "localStorage",
+          storeAuthStateInCookie: isIE,
+        },
+      }),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: ["user.read"],
+        },
+      },
+      {
+        interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
+        protectedResourceMap: new Map([
+          ["https://graph.microsoft.com/v1.0/me", ["user.read"]],
+        ]),
+      }
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    MsalGuard,
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
 export class AppModule { }
